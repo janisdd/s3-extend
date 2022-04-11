7bcf2b6d095108c6348978bd49e38899c857de22
Janis Dähne <janis.daehne@informatik.uni-halle.de>
2022-04-11 15:05:02 +0200

    - ir

--- s3_extend/gateways/rpi_gateway.py
+++ s3_extend/gateways/rpi_gateway.py
@@ -535,46 +535,51 @@ class RpiGateway(MyGatewayBase):
 
     def get_IR_key(self):
         IR = 17
-        if self.pi.read(IR) == 0:
-            count = 0
-            while self.pi.read(IR) == 0 and count < 200:  # 9ms
-                count += 1
-                time.sleep(0.00006)
-            if (count < 10):
-                return;
-            count = 0
-            while self.pi.read(IR) == 1 and count < 80:  # 4.5ms
-                count += 1
-                time.sleep(0.00006)
-
-            idx = 0
-            cnt = 0
-            data = [0, 0, 0, 0]
-            for i in range(0, 32):
+        n = 0
+        while n < 20_000:
+            n += 1
+            if self.pi.read(IR) == 0:
                 count = 0
-                while self.pi.read(IR) == 0 and count < 15:  # 0.56ms
+                while self.pi.read(IR) == 0 and count < 200:  # 9ms
+                    count += 1
+                    time.sleep(0.00006)
+                if (count < 10):
+                    return;
+                count = 0
+                while self.pi.read(IR) == 1 and count < 80:  # 4.5ms
                     count += 1
                     time.sleep(0.00006)
 
-                count = 0
-                while self.pi.read(IR) == 1 and count < 40:  # 0: 0.56mx
-                    count += 1  # 1: 1.69ms
-                    time.sleep(0.00006)
+                idx = 0
+                cnt = 0
+                data = [0, 0, 0, 0]
+                for i in range(0, 32):
+                    count = 0
+                    while self.pi.read(IR) == 0 and count < 15:  # 0.56ms
+                        count += 1
+                        time.sleep(0.00006)
 
-                if count > 7:
-                    data[idx] |= 1 << cnt
-                if cnt == 7:
-                    cnt = 0
-                    idx += 1
+                    count = 0
+                    while self.pi.read(IR) == 1 and count < 40:  # 0: 0.56mx
+                        count += 1  # 1: 1.69ms
+                        time.sleep(0.00006)
+
+                    if count > 7:
+                        data[idx] |= 1 << cnt
+                    if cnt == 7:
+                        cnt = 0
+                        idx += 1
+                    else:
+                        cnt += 1
+                #		print data
+                if data[0] + data[1] == 0xFF and data[2] + data[3] == 0xFF:  # check
+                    logToFile(f"IR read: {data[2]}")
+                    return data[2]
                 else:
-                    cnt += 1
-            #		print data
-            if data[0] + data[1] == 0xFF and data[2] + data[3] == 0xFF:  # check
-                logToFile(f"IR read: {data[2]}")
-                return data[2]
-            else:
-                logToFile(f"IR invalid read, setting to -1")
-                return -1
+                    logToFile(f"IR invalid read, setting to -1")
+                    return -1
+
+        return -1
 
 def rpi_gateway():
     parser = argparse.ArgumentParser()
